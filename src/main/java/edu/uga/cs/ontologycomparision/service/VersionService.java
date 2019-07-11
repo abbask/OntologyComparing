291762dce326e57b3045dae9dc064d20be2cf193
Abbas Keshavarzi <abbas.keshavarzi@gmail.com>
2019-07-11 17:39:01 -0400

    Added add new method to version service file.

--- src/main/java/edu/uga/cs/ontologycomparision/service/VersionService.java
+++ src/main/java/edu/uga/cs/ontologycomparision/service/VersionService.java
@@ -1,16 +1,21 @@
 package edu.uga.cs.ontologycomparision.service;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import edu.uga.cs.ontologycomparision.data.MySQLConnection;
 import edu.uga.cs.ontologycomparision.model.Version;
 
 public class VersionService {
+	
+	final static Logger logger = Logger.getLogger(VersionService.class);
 	
 	public List<Version> getListAll() throws SQLException {
 		
@@ -27,9 +32,39 @@ public class VersionService {
 		while(rs.next()) {
 			list.add(new Version(rs.getInt("ID"), rs.getString("name"), rs.getString("number")));
 		}
-		
+//		logger.info("VersionService.listAll : list version commited.");
 		return list;						
 				
+	}
+	
+	public void add(Version version) {
+		
+		MySQLConnection mySQLConnection = new MySQLConnection();
+		Connection c = mySQLConnection.openConnection();			
+		
+		try {
+			
+			String queryString = "INSERT INTO verion (name,number) VALUES (?,?)";
+			PreparedStatement statement= c.prepareStatement(queryString, Statement.RETURN_GENERATED_KEYS);
+			statement.setString(1,version.getName());
+			statement.setString(2,version.getNumber());
+			
+			statement.executeUpdate();
+			c.commit();
+			
+			c.close();
+			logger.info("VersionService.add : new version commited.");
+		} catch (Exception sqlException) {
+			try {
+				c.rollback();
+				logger.info("VersionService.add : new version is rolled back.");
+				c.close();
+			} catch (SQLException e) {
+				logger.error(e.getMessage(), e);
+			}
+			logger.error(sqlException.getMessage(), sqlException);
+		}
+		
 	}
 
 }
