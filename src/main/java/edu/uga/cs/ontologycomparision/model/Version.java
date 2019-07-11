7344b48b26804cd2d7ac71e5bf006ed1c7b281b8
Abbas Keshavarzi <abbas.keshavarzi@gmail.com>
2019-07-11 18:06:13 -0400

    modified the version model to have date

--- src/main/java/edu/uga/cs/ontologycomparision/model/Version.java
+++ src/main/java/edu/uga/cs/ontologycomparision/model/Version.java
@@ -1,20 +1,23 @@
 package edu.uga.cs.ontologycomparision.model;
 
+import java.sql.Date;;
+
 public class Version {
 
 	private int ID;
 	private String name;
 	private String number;
+	private Date date;
 	
 	public Version() {
 	}
 	
-	public Version(String name, String number) {
+	public Version(String name, String number, Date date) {
 		this.name = name;
 		this.number = number;
 	}
 	
-	public Version(int iD, String name, String number) {
+	public Version(int iD, String name, String number, Date date) {
 		ID = iD;
 		this.name = name;
 		this.number = number;
@@ -44,11 +47,20 @@ public class Version {
 		this.number = number;
 	}
 
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ID;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((number == null) ? 0 : number.hashCode());
 		return result;
@@ -65,6 +77,11 @@ public class Version {
 		Version other = (Version) obj;
 		if (ID != other.ID)
 			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
@@ -80,8 +97,10 @@ public class Version {
 
 	@Override
 	public String toString() {
-		return "Version [ID=" + ID + ", name=" + name + ", number=" + number + "]";
+		return "Version [ID=" + ID + ", name=" + name + ", number=" + number + ", date=" + date + "]";
 	}
+
+	
 	
 	
 	
