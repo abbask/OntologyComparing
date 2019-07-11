f82409367d4b4c40f72a94dac9df3da9c3b4da93
Abbas Keshavarzi <abbas.keshavarzi@gmail.com>
2019-07-11 16:58:38 -0400

    Added version entity model to project due to discussion with Dr. Kochut

---
+++ src/main/java/edu/uga/cs/ontologycomparision/model/Version.java
@@ -0,0 +1,88 @@
+package edu.uga.cs.ontologycomparision.model;
+
+public class Version {
+
+	private int ID;
+	private String name;
+	private String number;
+	
+	public Version() {
+	}
+	
+	public Version(String name, String number) {
+		this.name = name;
+		this.number = number;
+	}
+	
+	public Version(int iD, String name, String number) {
+		ID = iD;
+		this.name = name;
+		this.number = number;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ID;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((number == null) ? 0 : number.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Version other = (Version) obj;
+		if (ID != other.ID)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (number == null) {
+			if (other.number != null)
+				return false;
+		} else if (!number.equals(other.number))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Version [ID=" + ID + ", name=" + name + ", number=" + number + "]";
+	}
+	
+	
+	
+}
