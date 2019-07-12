a742de6c0e31325e8dc5daf37922c038af9b7ef0
Abbas Keshavarzi <abbas.keshavarzi@gmail.com>
2019-07-12 12:23:08 -0400

    src/test/java/

---
+++ src/test/java/TestXSDType.java
@@ -0,0 +1,33 @@
+import java.sql.SQLException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.uga.cs.ontologycomparision.model.XSDType;
+import edu.uga.cs.ontologycomparision.service.XSDTypeService;
+
+
+public class TestXSDType {
+	@Test
+	public void testAddAndGetByType() throws SQLException {
+		
+		XSDTypeService typeService = new XSDTypeService();
+		XSDType type = new XSDType();
+		
+		type.setType("test");
+		type.setUrl("http://test.com/");
+		
+        typeService.add(type);
+        
+        int id = type.getID();
+        Assert.assertNotNull(id);
+        
+        XSDType newType = typeService.getByType("test");
+        Assert.assertEquals("test", newType.getType());
+        Assert.assertEquals("http://test.com/",newType.getUrl());
+        
+        return;
+		
+	}
+	
+}
