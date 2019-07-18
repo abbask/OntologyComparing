8f5f47784d515add1c20960bbdd695c6b3548e6e
Abbas Keshavarzi <abbas.keshavarzi@gmail.com>
2019-07-18 09:57:40 -0400

    changed the way that retrieveObjectPropert get all the property to consider adding tripletypes too

--- src/main/java/edu/uga/cs/ontologycomparision/service/RetrieveSchemaService.java
+++ src/main/java/edu/uga/cs/ontologycomparision/service/RetrieveSchemaService.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.apache.jena.query.QuerySolution;
+import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.log4j.Logger;
@@ -90,32 +91,62 @@ public class RetrieveSchemaService {
 		Version version = versionService.get(versionId);
 		
 		DataStoreConnection conn = new DataStoreConnection(endpointURL, graphName);
-		List<QuerySolution> list = conn.executeSelect("PREFIX owl: <http://www.w3.org/2002/07/owl#> SELECT ?s FROM " + graphName + " WHERE{ ?s a owl:ObjectProperty.  }");
+		
+		
+		
+		String queryStringTriple = "PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+				+ "PREFIX owl:    <http://www.w3.org/2002/07/owl#> "
+				+ "PREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>";
+		
+		queryStringTriple += "SELECT DISTINCT ?domain ?name ?range (COUNT(?object) as ?count) "
+				+ "FROM " + graphName + "  "
+						+ "WHERE { ?name rdf:type owl:ObjectProperty "
+						+ "optional { ?name rdfs:domain ?o. ?o owl:unionOf ?l.  "
+						+ "{?l rdf:first ?domain. } UNION {?l rdf:rest ?rest. ?rest rdf:first ?domain}} "
+						+ "optional {?name rdfs:domain ?domain} "
+						+ "optional {?name rdfs:range ?range. ?range rdf:type owl:Class} "
+						+ "?subject ?name ?object} "
+						+ "GROUP By ?name ?domain ?range ORDER BY ?name";
 
+		List<QuerySolution> list = conn.executeSelect(queryStringTriple);
+		
 		for(QuerySolution soln : list) {
 			DataStoreConnection dataStoreConn = new DataStoreConnection(endpointURL, graphName);
 			
-			RDFNode predicate = soln.get("s");
-			Resource res = soln.getResource("s");
+			RDFNode predicate = soln.get("predicate");
+			Resource res = soln.getResource("predicate");
 
 			if (res.getLocalName() != null) {
 				//count instance of object property
-				String queryString = "PREFIX owl: <http://www.w3.org/2002/07/owl#> SELECT (count(?s) as ?Count) FROM " + graphName + " WHERE{ ?s <" + predicate + "> ?o .}";
-				List<QuerySolution> individuals =  dataStoreConn.executeSelect(queryString);
-				int count = individuals.get(0).get("Count").asLiteral().getInt();
+				Resource domainResource = soln.getResource("domain");
+				Resource rangeResource = soln.getResource("range");
+				Literal count = soln.getLiteral("count");
 				
 				//retrieve parent class
-				queryString = "PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> SELECT ?parent FROM " + graphName + " WHERE{ <" + predicate + "> rdfs:subPropertyOf ?parent .}";
+				String queryString = "PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> SELECT ?parent FROM " + graphName + " WHERE{ <" + predicate + "> rdfs:subPropertyOf ?parent .}";
 				List<QuerySolution> parents =  dataStoreConn.executeSelect(queryString);
 				Property parentProperty = null;
 				PropertyService propertyService = new PropertyService();
 				
 				if (parents.size() > 0) {
 					Resource parentResource = parents.get(0).getResource("parent");					
+					String parentURI = parentResource.getURI();
 					
+					String queryStringTripleParent = "PREFIX prokino: <http://om.cs.uga.edu/prokino/2.0/#>"
+							+ "PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+							+ "PREFIX owl:    <http://www.w3.org/2002/07/owl#> "
+							+ "PREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>";
 					
-					queryString = "PREFIX owl: <http://www.w3.org/2002/07/owl#> SELECT (count(?ind) as ?Count) FROM " + graphName + " WHERE{ ?s <" + parentResource.getURI() + "> ?o .}";
-					List<QuerySolution> parent =  dataStoreConn.executeSelect(queryString);
+					queryStringTripleParent += "SELECT DISTINCT ?domain ?range (COUNT(?object) as ?count) "
+							+ "FROM " + graphName + "  "
+									+ "WHERE { ?subject " + parentURI + " ?object "
+									+ "optional { " + parentURI + " rdfs:domain ?o. ?o owl:unionOf ?l.  "
+									+ "{?l rdf:first ?domain. } UNION {?l rdf:rest ?rest. ?rest rdf:first ?domain}} "
+									+ "optional {" + parentURI + " rdfs:domain ?domain} "
+									+ "optional {" + parentURI + " rdfs:range ?range. ?range rdf:type owl:Class} ";
+					
+//					
+					List<QuerySolution> parent =  dataStoreConn.executeSelect(queryStringTripleParent);
 					int parentCount = parent.get(0).get("Count").asLiteral().getInt();
 					
 					
@@ -124,7 +155,7 @@ public class RetrieveSchemaService {
 					parentProperty = propertyService.addIfNotExist(parentProperty);
 				}
 				
-				Property myProperty = new Property(res.getURI(), res.getLocalName(), "", count, version, parentProperty);
+				Property myProperty = new Property(res.getURI(), res.getLocalName(), "", count.getLong(), version, parentProperty);
 				myProperty = propertyService.addIfNotExist(myProperty);				
 				
 			}
