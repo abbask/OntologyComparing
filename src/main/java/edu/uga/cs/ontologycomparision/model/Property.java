827aa51c9ff19110a9f7345bd47f9a8f19139db6
Abbas Keshavarzi <abbas.keshavarzi@gmail.com>
2019-07-21 12:55:39 -0400

    fixed the equals and hashcode methods for property to fix the removal needed for comparison

--- src/main/java/edu/uga/cs/ontologycomparision/model/Property.java
+++ src/main/java/edu/uga/cs/ontologycomparision/model/Property.java
@@ -92,13 +92,9 @@ public class Property {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ID;
-		result = prime * result + ((comment == null) ? 0 : comment.hashCode());
+		
 		result = prime * result + ((label == null) ? 0 : label.hashCode());
-		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		result = prime * result + ((url == null) ? 0 : url.hashCode());
-		result = prime * result + ((version == null) ? 0 : version.hashCode());
+		
 		return result;
 	}
 
@@ -108,41 +104,15 @@ public class Property {
 			return true;
 		if (obj == null)
 			return false;
-		if (getClass() != obj.getClass())
-			return false;
+		
 		Property other = (Property) obj;
-		if (ID != other.ID)
-			return false;
-		if (comment == null) {
-			if (other.comment != null)
-				return false;
-		} else if (!comment.equals(other.comment))
-			return false;
+		
 		if (label == null) {
 			if (other.label != null)
 				return false;
 		} else if (!label.equals(other.label))
 			return false;
-		if (parent == null) {
-			if (other.parent != null)
-				return false;
-		} else if (!parent.equals(other.parent))
-			return false;
-		if (type == null) {
-			if (other.type != null)
-				return false;
-		} else if (!type.equals(other.type))
-			return false;
-		if (url == null) {
-			if (other.url != null)
-				return false;
-		} else if (!url.equals(other.url))
-			return false;
-		if (version == null) {
-			if (other.version != null)
-				return false;
-		} else if (!version.equals(other.version))
-			return false;
+		
 		return true;
 	}
 
