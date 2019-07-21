ff6cfefe26a3a059c743894862adbc3245c5781b
Abbas Keshavarzi <abbas.keshavarzi@gmail.com>
2019-07-21 10:59:19 -0400

    minor changes

--- src/test/java/CompareClasses.java
+++ src/test/java/CompareClasses.java
@@ -15,10 +15,10 @@ class CompareClasses {
 	void test() throws SQLException {
 		MySQLConnection mySQLConnection = new MySQLConnection();
 		VersionService service = new VersionService(mySQLConnection.openConnection());
-		Version ver1 = service.get(3);
+		Version ver1 = service.get(1);
 		Version ver2 = service.get(2);
 		CompareService compare = new CompareService(ver1, ver2);
-		System.out.println(compare.compareClasses().size());
+		System.out.println(compare.compareClasses());
 	}
 
 }
