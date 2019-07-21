fa593e13a782e460e64b03f7282bbf30e9d72de5
Abbas Keshavarzi <abbas.keshavarzi@gmail.com>
2019-07-21 12:49:01 -0400

    added compare object property

--- src/main/java/edu/uga/cs/ontologycomparision/service/CompareService.java
+++ src/main/java/edu/uga/cs/ontologycomparision/service/CompareService.java
@@ -14,6 +14,7 @@ import edu.uga.cs.ontologycomparision.model.Version;
 import edu.uga.cs.ontologycomparision.data.MySQLConnection;
 import edu.uga.cs.ontologycomparision.model.Result;
 import edu.uga.cs.ontologycomparision.model.Class;
+import edu.uga.cs.ontologycomparision.model.Property;
 
 public class CompareService {
 	
@@ -94,16 +95,9 @@ public class CompareService {
 		
 		Set<Class> class1SetTemp = new HashSet<Class>(class1Set);
 		
-		System.out.println("class1Set: " + class1Set.size());
-		System.out.println("class2Set: " + class2Set.size());
-		System.out.println("class1SetTemp: " + class2Set.size());
-		
 		class1Set.removeAll(class2Set);
 		class2Set.removeAll(class1SetTemp);
 		
-		System.out.println("After Removal:");
-		System.out.println("class1Set: " + class1Set.size());
-		System.out.println("class2Set: " + class2Set.size());
 		
 		List<Result<String, String>> class1List = class1Set.stream()
 				.map(myClass -> new Result<String, String>(myClass.getLabel(), "Only in Version " + ver1.getNumber()))
@@ -116,6 +110,33 @@ public class CompareService {
 		List<Result<String, String>> result = new ArrayList<>();
 		result.addAll(class1List);
 		result.addAll(class2List);
+
+		return result;
+	}
+	
+	public List<Result<String, String>> compareObjectProperties() throws SQLException{
+		PropertyService service = new PropertyService(connection, ObjectPropertyType);
+
+		Set<Property> property1Set = service.listAll(ver1.getID()).stream().collect(Collectors.toSet());
+		Set<Property> property2Set = service.listAll(ver2.getID()).stream().collect(Collectors.toSet());
+		
+		Set<Property> property1SetTemp = new HashSet<Property>(property1Set);
+		
+		property1Set.removeAll(property2Set);
+		property2Set.removeAll(property1SetTemp);
+		
+		
+		List<Result<String, String>> property1List = property1Set.stream()
+				.map(myProperty -> new Result<String, String>(myProperty.getLabel(), "Only in Version " + ver1.getNumber()))
+				.collect(Collectors.toList());
+		
+		List<Result<String, String>> property2List = property2Set.stream()
+				.map(myProperty -> new Result<String, String>(myProperty.getLabel(), "Only in Version " + ver2.getNumber()))
+				.collect(Collectors.toList());
+				
+		List<Result<String, String>> result = new ArrayList<>();
+		result.addAll(property1List);
+		result.addAll(property2List);
 
 		return result;
 	}
