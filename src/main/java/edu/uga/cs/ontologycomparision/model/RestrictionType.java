b24c1925a1370098e88c4662ed321a2b4053abaa
Abbas Keshavarzi <abbas.keshavarzi@gmail.com>
2019-08-21 11:36:02 -0400

    changed the restriction type from enum to class to have Id and type string for ease of development

--- src/main/java/edu/uga/cs/ontologycomparision/model/RestrictionType.java
+++ src/main/java/edu/uga/cs/ontologycomparision/model/RestrictionType.java
@@ -1,17 +1,77 @@
 package edu.uga.cs.ontologycomparision.model;
 
-public enum RestrictionType {
+public class RestrictionType {
 	
-	someValuesFrom,
-	allValuesFrom,
-	hasValue,
-	hasSelf,
-	minCardinality, 
-	maxCardinality, 
-	cardinality, 
-	minQualifiedCardinality,
-	maxQualifiedCardinality,
-	qualifiedCardinality,
+	private int ID;
+	private String type;
+	
+	public RestrictionType() {
+	
+	}
+	
+	public RestrictionType(int iD, String type) {
+		super();
+		ID = iD;
+		this.type = type;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RestrictionType other = (RestrictionType) obj;
+		if (ID != other.ID)
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "RestrictionType [ID=" + ID + ", type=" + type + "]";
+	}
 	
 }
-///qualifiedCardinality	
+//someValuesFrom,
+//allValuesFrom,
+//hasValue,
+//hasSelf,
+//minCardinality, 
+//maxCardinality, 
+//cardinality, 
+//minQualifiedCardinality,
+//maxQualifiedCardinality,
+//qualifiedCardinality,	
