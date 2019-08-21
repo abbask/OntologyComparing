dafde6072bbe3c92f927fd2b2100351d5cc2db99
Abbas Keshavarzi <abbas.keshavarzi@gmail.com>
2019-08-21 16:30:03 -0400

    added getByType to restriction type service

--- src/main/java/edu/uga/cs/ontologycomparision/service/RestrictionTypeService.java
+++ src/main/java/edu/uga/cs/ontologycomparision/service/RestrictionTypeService.java
@@ -9,7 +9,9 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
+import edu.uga.cs.ontologycomparision.model.Class;
 import edu.uga.cs.ontologycomparision.model.RestrictionType;
+import edu.uga.cs.ontologycomparision.model.Version;
 
 public class RestrictionTypeService {
 	
@@ -35,6 +37,50 @@ final static Logger logger = Logger.getLogger(RestrictionTypeService.class);
 		}
 		
 		return list.get(0);				
+	}
+	
+	public List<RestrictionType> getListAll() throws SQLException {
+		
+		List<RestrictionType> list = new LinkedList<RestrictionType>();		
+		
+		connection.setAutoCommit(false);
+		Statement stmtSys = connection.createStatement();			
+		String query = "SELECT * FROM restriction_type ORDER BY ID";
+		ResultSet rs = stmtSys.executeQuery(query); 
+		
+		while(rs.next()) {
+			list.add(new RestrictionType(rs.getInt("ID"), rs.getString("type")));
+		}
+		return list;						
+				
+	}
+	
+	public RestrictionType getByType(String type) throws SQLException {
+		
+		List<RestrictionType> list = new LinkedList<RestrictionType>();
+				
+		
+		Statement stmtSys = connection.createStatement();			
+		String query = "SELECT * FROM restriction_type where type='" + type + "'";
+		ResultSet rs = stmtSys.executeQuery(query); 
+			
+		
+		while(rs.next()) {
+			
+			RestrictionType retrictionType = new RestrictionType(rs.getInt("version_id"), type);
+			
+			list.add(retrictionType);
+		}
+		RestrictionType result ;
+		if (list.size() > 0) 
+			result = list.get(0);
+		else
+			result = null;
+		
+		
+		logger.info("RestrictionTypeService.getByType : retrieved Restriction Type.");
+		return result;					
+				
 	}
 
 }
