0fab17735862d290fe4ffc74cfff26d39ebbbfab
Abbas Keshavarzi <abbas.keshavarzi@gmail.com>
2019-08-23 11:16:44 -0400

    modified test to test the parse json along with http connection

--- src/test/java/HTTPConnectionTest.java
+++ src/test/java/HTTPConnectionTest.java
@@ -1,20 +1,28 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import org.junit.jupiter.api.Test;
 
 import edu.uga.cs.ontologycomparision.data.HTTPConnection;
+import edu.uga.cs.ontologycomparision.service.RetrieveSchemaService;
 
 class HTTPConnectionTest {
 
 	@Test
-	void test() throws IOException {
+	void test() throws IOException, SQLException {
 		String endPoint = "http://vulcan.cs.uga.edu:8890/sparql/";
 		String sparqlQuery = "select * where {?s ?p ?o. FILTER( ?p IN(owl:unionOf, owl:intersectionOf) ) } LIMIT 100";
 		
 		HTTPConnection connection = new HTTPConnection(endPoint, sparqlQuery);
-		System.out.println(connection.execute());
+		String result = connection.execute();
+		RetrieveSchemaService service = new RetrieveSchemaService("", "");
+		service.parseJson(result);
+		
+		
 	}
+	
+	
 
 }
