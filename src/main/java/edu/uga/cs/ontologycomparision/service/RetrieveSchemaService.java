af943d7e32fadfcc205b0c6b9696216ebb46305a
Abbas Keshavarzi <abbas.keshavarzi@gmail.com>
2019-08-26 11:29:49 -0400

    fixed the retrieve schema for getting propert and class for expressions

--- src/main/java/edu/uga/cs/ontologycomparision/service/RetrieveSchemaService.java
+++ src/main/java/edu/uga/cs/ontologycomparision/service/RetrieveSchemaService.java
@@ -202,17 +202,20 @@ public class RetrieveSchemaService {
 		
 		DataStoreConnection conn = new DataStoreConnection(endpointURL, graphName);
 		
-		String queryString = "PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> SELECT ?parent ?p"
-				+ (graphName.isBlank()? "" : "FROM " + graphName) + " WHERE{ <" + propertyString + "> rdfs:subPropertyOf ?parent .bind(<" + propertyString + "> AS ?p)}";
+		String queryString = "PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> SELECT ?parent ?p "
+				+ (graphName.isBlank()? "" : "FROM " + graphName) + " WHERE{ optional{<" + propertyString + "> rdfs:subPropertyOf ?parent} .bind(<" + propertyString + "> AS ?p)}";
+		
 		List<QuerySolution> parents =  conn.executeSelect(queryString);
 		
 		Property parentProperty = null;	
-		if (parents.size() > 0) {
+		
+		Resource propertyResource = parents.get(0).get("p").asResource();
+		RDFNode parentNode = parents.get(0).get("parent");
+		
+		if (parentNode != null) {
 			
 			parentProperty = collectProperty(parents.get(0).get("parent").toString(), type);
 		}
-				
-		Resource propertyResource = parents.get(0).get("p").asResource();
 				
 		PropertyService propertyService = new PropertyService(connection);												
 		Property myProperty = new Property(propertyResource.getURI(), propertyResource.getLocalName(),type, "", version, parentProperty);
